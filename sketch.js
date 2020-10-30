a6b0357c2a09e11ca9f8665441b71f7405cd01c7
ftg-gtf <67550388+ftg-gtf@users.noreply.github.com>
2020-10-30 19:22:53 +0530

    Changed random and speed,weight in sketch.js.

--- sketch.js
+++ sketch.js
@@ -3,22 +3,25 @@ var speed,weight,deformation ;
 
 function setup() {
   createCanvas(1600,400);
-  createSprite(200,200,50,50);
+  //createSprite(200,200,50,50);
+
+  speed=random(55,90);
+weight=random(400,1500);
+
   car = createSprite(50, 200, 50, 50);
   car.velocityX = speed ;
 
   wall = createSprite(1500,200,60,height/2);
   wall.shapeColor=color(80,80,80);
   
-speed=Random(55,90);
-weight=Random(400,1500);
+
 
 }
 
 function draw() {
-  background(255,255,255);
+  background(0);
 
-  if (wall.x - car.x < (car.width/wall.width/2 ))
+  if (wall.x - car.x < (car.width+wall.width/2 ))
 {
 car.velocityX=0;
 var deformation=0.5 * weight * speed * speed/22500 ;
